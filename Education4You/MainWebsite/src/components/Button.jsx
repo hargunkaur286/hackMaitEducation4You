4f7edacb372b860f5079bdaf46718d1c2a47346f
Hargun Kaur <hargunkaur286@gmail.com>
2023-11-23 20:41:47 +0530

    added main website

---
+++ Education4You/MainWebsite/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Button = ({ styles }) => (
+  <a href="https://655ef857e3d8f24eda9cf6b5--coruscating-licorice-821e81.netlify.app/">
+    <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
+    TEZOS
+  </button>
+  </a>
+  
+);
+
+export default Button;
